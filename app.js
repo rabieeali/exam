196f29f2d86c329fbc5984dce68941c8f6209cd2
alirbi <alirbi.webdev@gmail.com>
2022-01-19 20:12:54 +0330

    two solutions

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// let chances = {
-//   1: " It is certain.",
-//   2: " It is decidedly so.",
-//   3: " Without a doubt.",
-//   4: "Yes definitely.",
-//   5: " You may rely on it.",
-//   6: " As I see it, yes.",
-//   7: " Most likely.",
-//   8: "Outlook good.",
-//   9: " Yes.",
-//   10: " Signs point to yes.",
-//   11: "Reply hazy, try again.",
-//   12: " Ask again later.",
-//   13: "Better not tell you now.",
-//   14: "Cannot predict now.",
-//   15: "Concentrate and ask again.",
-//   16: "Don't count on it.",
-//   17: "My reply is no.",
-//   18: "My sources say no.",
-//   19: "Outlook not so good.",
-//   20: " Very doubtful.",
-// };
 
 let chances = [
   "It is certain.",
@@ -41,22 +19,37 @@ let chances = [
   "My reply is no.",
   "My sources say no.",
   "Outlook not so good.",
-  "Very doubtful."
+  "Very doubtful.",
 ];
 
 
-function checkChances(chances) {
-    
-    let randomNumber = Math.floor(Math.random() * 20 + 1);
-    if (randomNumber <= 10) {
-        console.log(chances[randomNumber], "green");
-    } else if (randomNumber <= 15) {
-        console.log(chances[randomNumber], "yellow");
-    } else {
-        console.log(chances[randomNumber], "red");
-    }
-}
-checkChances(chances);
+//***************************************************** Solution 1 : if it's an Array ********************************************
 
+// checkChances = (chances) => {
+//     let randomNumber = Math.floor(Math.random() * 20 + 1);
+//     randomNumber <= 10
+//       ? console.log(chances[randomNumber], "positive")
+//       : randomNumber <= 15
+//       ? console.log(chances[randomNumber], "natural")
+//       : console.log(chances[randomNumber], "negative");
+//   };
+  
+//   checkChances(chances);
+
+//***************************************************** */ Solution 2 : if it's an Object ***************************************************
+
+// let chancesArr = { ...chances };
+// fortunTeller = (chancesArr) => {
+//     let randomNumber = Math.floor(Math.random() * 20 + 1);
+//     let answer = chances[randomNumber];
+//     console.log(answer);
+    
+//     randomNumber <= 10
+//     ? console.log("positive")
+//     : randomNumber <= 15
+//     ? console.log("natural")
+//     : console.log("negative");
+// };
+// fortunTeller(chancesArr);
 
 
